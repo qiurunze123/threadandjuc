f978e69dd84a01626a7bbb65aedfa7b648275493
qiurunze <qiurunze@youxin.com>
2018-10-25 20:13:17 +0800

    future模式 -- --  例子与说明

---
+++ src/main/java/com/geek/threadandjuc/juc/future/CookFuture.java
@@ -0,0 +1,88 @@
+package com.geek.threadandjuc.juc.future;
+
+import org.joda.time.DateTime;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+//线程异步
+//这个继承体系中的核心接口是Future。Future的核心思想是：一个方法f，计算过程可能非常耗时，等待f返回，显然不明智。可以在调用f的时候，
+// 立马返回一个Future，可以通过Future这个数据结构去控制方法f的计算过程。
+//这里的控制包括：
+//get方法：获取计算结果（如果还没计算完，也是必须等待的）
+//cancel方法：还没计算完，可以取消计算过程
+//isDone方法：判断是否计算完
+//isCancelled方法：判断计算是否被取消
+//这些接口的设计很完美，FutureTask的实现注定不会简单，后面再说。
+public class CookFuture {
+    public static void main(String[] args) {
+        System.out.println(new DateTime().withTimeAtStartOfDay().toDate());
+
+        long startTime = System.currentTimeMillis();//计算开始时间
+
+//        Callable<Chuju>  onlineBuyCook = new Callable<Chuju>() {
+//            @Override
+//            public Chuju call() throws Exception {
+//                System.out.println("第一步：下单！");
+//                System.out.println("第二步：等待送货！");
+//                Thread.sleep(5000);//5秒送到
+//                System.out.println("第三步快递到家！");
+//                return new Chuju();
+//            }
+//        };
+
+        FutureTask<Chuju> task = new FutureTask<Chuju>(new OnlineBuyCook());
+
+        new Thread(task).start();
+
+        // 第二步 去超市购买食材
+        try {
+            Thread.sleep(2000);  // 模拟购买食材时间
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Shicai shicai = new Shicai();
+        System.out.println("第二步：食材到位");
+
+        //第三步 用厨具烹饪食物
+        if(!task.isDone()){
+            System.out.println("厨具是否到货！ 进行下一步操作！");
+        }
+
+        Chuju chuju =null;
+        try {
+             chuju = task.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        System.out.println("厨具到货展现厨艺！");
+
+        Cook(chuju,shicai);
+
+        System.out.println("总共用时："+ (System.currentTimeMillis()-startTime)+"ms");
+    }
+
+    static void Cook(Chuju chuju,Shicai shicai){}
+
+    //厨具
+    static class Chuju{}
+
+    //食材
+    static class Shicai{}
+
+
+    public static class OnlineBuyCook implements Callable<Chuju>{
+
+        @Override
+        public Chuju call() throws Exception {
+            System.out.println("第一步：下单！");
+            System.out.println("第二步：等待送货！");
+            Thread.sleep(5000);//5秒送到
+            System.out.println("第三步快递到家！");
+            return new Chuju();
+        }
+    }
+}
---
+++ src/main/java/com/geek/threadandjuc/juc/future/futuretype/JdkFuture.java
@@ -0,0 +1,66 @@
+package com.geek.threadandjuc.juc.future.futuretype;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+/**
+* @author: Kevin
+* @官网: 	www.mimaxueyuan.com
+* @Q Q群:	660567408
+* @Email:	mimaxueyuan@163.com
+* [每天进步一点点、人生带来大改变...]
+* [本代码对应视频地址:http://study.163.com/course/introduction/1004176043.htm]
+*/
+public class JdkFuture implements Callable<String>{
+	private String para;
+	
+	public JdkFuture(String para){
+		this.para = para;
+	}
+	
+	/**
+	 * 这里是真实的业务逻辑，其执行可能很慢
+	 */
+	@Override
+	public String call() throws Exception {
+		//模拟执行耗时
+		Thread.sleep(5000);
+		String result = this.para + "处理完成";
+		return result;
+	}
+	
+	//主控制函数
+	public static void main(String[] args) throws Exception {
+		String queryStr = "zhangsan";
+		
+		//构造FutureTask，并且传入需要真正进行业务逻辑处理的类,该类一定是实现了Callable接口的类
+		FutureTask<String> future = new FutureTask<String>(new JdkFuture(queryStr));
+		FutureTask<String> future2 = new FutureTask<String>(new JdkFuture(queryStr));
+		
+		//创建一个固定线程的线程池且线程数为1,
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		//这里提交任务future,则开启线程执行RealData的call()方法执行
+		//submit和execute的区别： 第一点是submit可以传入实现Callable接口的实例对象， 第二点是submit方法有返回值
+		
+		Future f1 = executor.submit(future);		//单独启动一个线程去执行的
+		Future f2 = executor.submit(future2);
+		System.out.println("请求完毕");
+		
+		try {
+			//这里可以做额外的数据操作，也就是主程序执行其他业务逻辑
+			System.out.println("处理实际的业务逻辑...");
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//调用获取数据方法,如果call()方法没有执行完成,则依然会进行等待
+		System.out.println("数据：" + future.get());
+		System.out.println("数据：" + future2.get());
+		
+		executor.shutdown();
+	}
+
+}
