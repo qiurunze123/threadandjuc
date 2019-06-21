a42aef77d99b3f7bd7dc6bef23de2f637f914afc
qiurunze <qiurunze@youxin.com>
2019-06-21 18:35:43 +0800

    提交

--- src/main/java/com/geek/CASDemo/CasDemo.java
+++ src/main/java/com/geek/CASDemo/CasDemo.java
@@ -5,12 +5,20 @@ import java.util.concurrent.atomic.AtomicInteger;
 /**
  * @author 邱润泽 bullock
  *
- * 什么是CAS ??
+ * 什么是CAS ?? compare and swap  比较和设值
+ *
+ * 比较并交换
  */
 public class CasDemo {
 
     public static void main(String[] args) {
 
         AtomicInteger atomicInteger = new AtomicInteger(5);
+
+        System.out.println(atomicInteger.compareAndSet(5,2019));
+        System.out.println(atomicInteger.compareAndSet(5,2020));
+
+        System.out.println(atomicInteger.get());
+
     }
 }
