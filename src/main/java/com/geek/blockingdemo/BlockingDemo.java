f11e3bde83bc6332a629dc6d9f7dc67ea1caf5eb
qiurunze <qiurunze@youxin.com>
2019-06-26 11:12:02 +0800

    阻塞队列

---
+++ src/main/java/com/geek/blockingdemo/BlockingDemo.java
@@ -0,0 +1,14 @@
+package com.geek.blockingdemo;
+
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * @author 邱润泽 bullock
+ */
+public class BlockingDemo {
+
+    public static void main(String[] args) {
+        BlockingDeque blockingDeque = new LinkedBlockingDeque();
+    }
+}
