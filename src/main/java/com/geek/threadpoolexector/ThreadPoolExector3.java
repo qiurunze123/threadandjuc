a0801b17fd21b4a7a28aa8b3c0f76616d56b7fbf
qiurunze <qiurunze@youxin.com>
2019-07-03 17:30:19 +0800

    tp

--- src/main/java/com/geek/threadpoolexector/ThreadPoolExector3.java
+++ src/main/java/com/geek/threadpoolexector/ThreadPoolExector3.java
@@ -10,16 +10,5 @@ public class ThreadPoolExector3 {
 
     public static void main(String[] args) {
 
-        ExecutorService service1 = Executors.newSingleThreadExecutor();
-
-
-        Thread t1 =  new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("我是多线程!!");
-            }
-        });
-
-        service1.execute(t1);
     }
 }
