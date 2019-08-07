b2be9b7d030e79e8f507cd76e2cfbbb244af9ff6
qiurunze <qiurunze@youxin.com>
2019-08-07 19:12:28 +0800

    提交\

---
+++ src/main/java/com/geek/falsesharing/FalseSharingtest.java
@@ -0,0 +1,52 @@
+package com.geek.falsesharing;
+
+/**
+ * @author 邱润泽 bullock
+ */
+public class FalseSharingtest {
+
+    public static void main(String[] args) throws InterruptedException {
+        testPointer(new Pointer());
+    }
+
+    private static void testPointer(Pointer pointer) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100000000; i++) {
+//                pointer.x++;
+                pointer.x.value++;
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100000000; i++) {
+//                pointer.y++;
+                pointer.y.value++;
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println(System.currentTimeMillis() - start);
+        System.out.println(pointer);
+    }
+}
+
+//class Pointer {
+//    volatile long x;
+//    long p1, p2, p3, p4, p5, p6, p7;
+//    volatile long y;
+//}
+
+class Pointer {
+    MyLong x = new MyLong();
+    MyLong y = new MyLong();
+}
+
+class MyLong {
+    volatile long value;
+    long p1, p2, p3, p4, p5, p6, p7;
+}
