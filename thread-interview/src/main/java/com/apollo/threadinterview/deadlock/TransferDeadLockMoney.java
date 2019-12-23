4a41321f035f569af371b53d1aa9ddf9624c1cda
qiurunze <qiurunze@youxin.com>
2019-12-23 19:46:29 +0800

    提交转账死锁

---
+++ thread-interview/src/main/java/com/apollo/threadinterview/deadlock/TransferDeadLockMoney.java
@@ -0,0 +1,13 @@
+package com.apollo.threadinterview.deadlock;
+
+/**
+ * @author 邱润泽 bullock
+ */
+public class TransferDeadLockMoney implements Runnable {
+
+
+    @Override
+    public void run() {
+
+    }
+}
