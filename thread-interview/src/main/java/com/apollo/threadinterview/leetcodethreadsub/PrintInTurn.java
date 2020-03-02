e82ceb17f57fefc333ce21b1f43303b8cafc0a2d
qiurunze <qiurunze@youxin.com>
2020-03-02 14:25:41 +0800

    提交 线程池

---
+++ thread-interview/src/main/java/com/apollo/threadinterview/leetcodethreadsub/PrintInTurn.java
@@ -0,0 +1,99 @@
+package com.apollo.threadinterview.leetcodethreadsub;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author 邱润泽 bullock
+ * <p>
+ * 三个不同的线程将会共用一个 PrintInTurn 实例。
+ * <p>
+ * 线程 A 将会调用 one() 方法
+ * 线程 B 将会调用 two() 方法
+ * 线程 C 将会调用 three() 方法
+ * 请设计修改程序，以确保 two() 方法在 one() 方法之后被执行，three() 方法在 two() 方法之后被执行。
+ */
+public class PrintInTurn {
+
+
+    public static void main(String[] args) {
+
+        PrintInturnTherad ad = new PrintInturnTherad();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ad.first(new Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("我来打印1");
+                    }
+                });
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ad.second(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("我来打印2");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ad.third(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("我来打印3");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t2.start();
+        t1.start();
+        t3.start();
+    }
+
+
+}
+
+class PrintInturnTherad {
+
+    private CountDownLatch countDownLatchA;
+    private CountDownLatch countDownLatchB;
+
+    public PrintInturnTherad() {
+        countDownLatchA = new CountDownLatch(1);
+        countDownLatchB = new CountDownLatch(1);
+    }
+
+    public void first(Runnable printFirst) {
+        printFirst.run();
+        countDownLatchA.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        countDownLatchA.await();
+        printSecond.run();
+        countDownLatchB.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        // printThird.run() outputs "third". Do not change or remove this line.
+        countDownLatchB.await();
+        printThird.run();
+    }
+
+}
---
+++ thread-interview/src/main/java/com/apollo/threadinterview/leetcodethreadsub/SemaphoreInTurn.java
@@ -0,0 +1,137 @@
+package com.apollo.threadinterview.leetcodethreadsub;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author 邱润泽 bullock
+ */
+public class SemaphoreInTurn {
+
+    public static void main(String[] args) {
+
+        Foo03 ad = new Foo03();
+
+        ExecutorService service = Executors.newFixedThreadPool(3);
+
+        service.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ad.first(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("我来打印1");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        service.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ad.second(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("我来打印2");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ad.first(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("我来打印1");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ad.second(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("我来打印2");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ad.third(new Runnable() {
+                        @Override
+                        public void run() {
+                            System.out.println("我来打印3");
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t2.start();
+        t1.start();
+        t3.start();
+
+
+
+    }
+}
+
+class Foo03{
+
+    private Semaphore spa,spb;
+
+    public Foo03(){
+        spa = new Semaphore(0);
+        spb = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        //只有等first线程释放Semaphore后使Semaphore值为1,另外一个线程才可以调用（acquire）
+        spa.release();
+    }
+    public void second(Runnable printSecond) throws InterruptedException {
+        //只有spa为1才能执行acquire，如果为0就会产生阻塞
+        spa.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        spb.release();
+    }
+    public void third(Runnable printThird) throws InterruptedException {
+        //只有spb为1才能通过，如果为0就会阻塞
+        spb.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+
+
+}
